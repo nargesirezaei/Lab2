7d5488a2af1572b2b51765fcad4090752b67c60c
nargesirzi <71456874+nargesirezaei@users.noreply.github.com>
2023-02-08 14:24:13 +0100

    added some explanation

--- client.py
+++ client.py
@@ -1,9 +1,20 @@
 import socket
+#lager en socket for client og setter vi til to parameter som utgjør typen av socket-en
+# socket.AF_INET => indicates that underlying network is using IPv4
+#socket.SOCK_STREAM => indicates that it is a TCP socket
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-#connecting client to server
-s.connect((socket.gethostname(),1212))
 
-#when we using socket.SOCK_STREAM , so we should identify size of bytes we want to recieve
-msg = s.recv(1024)
-print(msg.decode("utf-8"))
+#binder client socket to server med to parameteret. første er server hostname, og den andre er det portet som 
+#server bruker til å ta i mot forespørsel fra client
+#i den tilfelle både server og client er samme så host name blir også det samme
+s.connect((socket.gethostname(),1234))
+
+
+full_msg = ''
+while True:
+    msg = msg.rec(8)
+    if len(msg) <= 0:
+        break
+    full_msg += msg.decode("utf-8")
+    print(full_msg)
--- server.py
+++ server.py
@@ -1,16 +1,24 @@
 import socket;
-#defining our client-socket
+##lager en socket for client og setter vi til to parameter som utgjør typen av socket-en
+# socket.AF_INET => indicates that underlying network is using IPv4
+#socket.SOCK_STREAM => indicates that it is a TCP socket
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-#binding, telling which local protocol liten to
-s.bind((socket.gethostname(),1212))
+#binding server socket til et spesific port som server bruker til å ta i mot requset fra client
+s.bind((socket.gethostname(),1234))
+
+#server will preapre a queue of 5
 s.listen(5)
 
+
 while True:
-    #connenct whatever connection from client
-    #address is client ip address, and clientsocket is object
+    #alle klineter som har med hostname og addresse, dvs. IP og portnummer skal få koble seg med server
     clientsocket, address = s.accept()
+     
+    #once we got that connenction, printing out this msg
     print(f"connection from {address} has been stablished! ")
+    
+    #then send we this msg to client-socket
     clientsocket.send(bytes("welcome to the server","utf-8"))
     clientsocket.close()
     
